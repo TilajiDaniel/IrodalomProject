a3160ee1a590a0d8eebaa5a16e860b4f84194b80
TilajiDG <tilajid@kkszki.hu>
2025-02-17 09:01:56 +0100

    class letrehozasa és propeti

---
+++ IrodalomProject/Models/Kerdes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrodalomProject.Models
+{
+    internal class Kerdes
+    {
+        public Kerdes(string kerdesSzovege, string valaszA, string valaszB, string valaszC, string helyesvalasz)
+        {
+            KerdesSzovege = kerdesSzovege;
+            ValaszA = valaszA;
+            ValaszB = valaszB;
+            ValaszC = valaszC;
+            Helyesvalasz = helyesvalasz;
+        }
+
+        public string KerdesSzovege { get; set; }
+        public string ValaszA { get; set; }
+        public string ValaszB { get; set; }
+        public string ValaszC { get; set; }
+        public string Helyesvalasz { get; set; }
+        public string? FelhasznaloValasza { get; set; }
+
+
+
+    }
+}
---
+++ IrodalomProject/Models/Valasz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrodalomProject.Models
+{
+    internal class Valasz
+    {
+        public string KerdesSzoveg { get; set; } = KerdesSzoveg;
+        public string? ValaszBetujele { get; set; }
+        public bool
+    }
+}
